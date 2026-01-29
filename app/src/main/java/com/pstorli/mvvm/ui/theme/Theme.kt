d037b388dcd7365f680f5422548afe62d92858e9
PETER STORLI <pstorli@gmail.com>
2026-01-28 18:56:51 -0800

    Updated readme with link https://drive.google.com/file/d/1zy6H26v2cxQEe-QxTWSUvlI4tQqDctkJ/view?usp=sharing

--- app/src/main/java/com/pstorli/mvvm/ui/theme/Theme.kt
+++ app/src/main/java/com/pstorli/mvvm/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 @file:Suppress("unused")
 package com.pstorli.mvvm.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
